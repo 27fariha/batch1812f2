0c7090028f5b70239af32d5f86fdc10b63b8f9aa
fariha <fariha_ahmed27@hotmail.com>
2020-11-24 17:29:56 +0500

    Firebase Login

---
+++ firebaseLogin/app/src/main/java/com/example/firebaselogin/Dashboard.java
@@ -0,0 +1,30 @@
+package com.example.firebaselogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Dashboard extends AppCompatActivity {
+Button logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        logout=findViewById(R.id.button);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(Dashboard.this,"Logout App",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(Dashboard.this,MainActivity.class));
+            }
+        });
+    }
+}
